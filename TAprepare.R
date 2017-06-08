d90fc4cd680cf6da24b2a5dd0a2e1fce9420599f
jernejk7 <jernej.knehtl@gmail.com>
2017-06-08 18:53:34 +0200

    Add files via upload

---
+++ TAprepare.R
@@ -0,0 +1,21 @@
+tabela1<-read.table("C:/Users/Jerry/Documents/ekoF/DataM/Tripadvisor project/Tripadvisor_project/TAOutput.txt", header=TRUE, sep="|",quote="" ,stringsAsFactors=FALSE, fill = TRUE, check.names=FALSE )
+
+
+tabela2<-read.table("C:/Users/Jerry/Documents/ekoF/DataM/Tripadvisor project/Tripadvisor_project/hoteli_lok_.txt", header=TRUE, sep=";", quote = "", fill = TRUE, stringsAsFactors=FALSE, check.names=FALSE )
+
+library("dplyr", lib.loc="~/R/win-library/3.4")
+
+
+#inner join 
+tabela3<-inner_join(tabela1,tabela2,by="Hotel") 
+#tabela4<-merge(x=tabela1, y=tabela2, by="Hotel")
+
+print(head(tabela1))
+print(tabela2)
+print(head(tabela3))
+print(tail(tabela3))
+summary(tabela3)
+unique(print(tabela3$Hotel))
+
+
+write.table(tabela3,"hoteliTA",sep ="|", row.names=FALSE ,quote=F)
