dbec05f6bce5525c67a1f0d1de6f73a9d32ed63b
jernejk7 <jernej.knehtl@gmail.com>
2017-06-12 18:03:35 +0200

    Add files via upload

--- TA_zemljevid.R
+++ TA_zemljevid.R
@@ -1,20 +1,23 @@
-tabela<-read.csv(file="TA.csv", header=TRUE, sep=";" ,stringsAsFactors=FALSE, dec=",", row.names=NULL)
-tabela$Lat <- NULL
-tabela$Long <- NULL
-tabela2<-read.csv("C:/Users/Jerry/Documents/ekoF/DataM/Tripadvisor project/Tripadvisor_project/hoteli_lokacija_.txt", header=TRUE, sep=";", quote = "", fill = TRUE, stringsAsFactors=FALSE, check.names=FALSE )
 library("dplyr", lib.loc="~/R/win-library/3.4")
 library(ggmap)
 library(ggplot2)
 
+
+tabela<-read.csv(file="TA.csv", header=TRUE, sep=";" ,stringsAsFactors=FALSE, dec=",", row.names=NULL)
+tabela$Lat <- NULL
+tabela$Long <- NULL
+tabela2<-read.csv("C:/Users/Jerry/Documents/ekoF/DataM/Tripadvisor project/Tripadvisor_project/hoteli_lokacija_.txt", header=TRUE, sep=";", quote = "", fill = TRUE, stringsAsFactors=FALSE, check.names=FALSE )
+
 tabela3<-inner_join(tabela,tabela2,by="Hotel") 
-print(head(tabela3))
-print(head(tabela))
-print(head(tabela2))
+# print(head(tabela3))
+# print(head(tabela))
+# print(head(tabela2))
 map<-get_map(location='Slovenia', zoom=8, maptype = "terrain", source='google',color='color')
 
-ggmap(map) +
-  geom_point(data = tabela3, aes(x = Long, y = Lat, fill = "red", alpha = 0.8), size = 3, shape = 21) +
-  guides(fill=FALSE, alpha=FALSE, size=FALSE)
+# Za celotne podatke v eni barvi
+# ggmap(map) +
+#   geom_point(data = tabela3, aes(x = Long, y = Lat, fill = "red", alpha = 0.8), size = 3, shape = 21) +
+#   guides(fill=FALSE, alpha=FALSE, size=FALSE)
 
 
 
@@ -25,7 +28,7 @@ dataski
 data_spa<- filter(tabela3, Location =="Spa")
 dataspa
 
-
+# grupirani hoteli glede na lokacijo v treh različnih barvah 
 ggmap(map) +
   geom_point(data = data_ski, aes(x = Long, y = Lat, fill = "red", alpha = 0.8), size = 3, shape = 21) +
   guides(fill=FALSE, alpha=FALSE, size=FALSE) + geom_point(data = data_sea, aes(x = Long, y = Lat, fill = "yellow", alpha = 0.8), size = 3, shape = 21) +
